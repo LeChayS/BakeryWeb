9553cd2e2943738097c91053cb897bd36220e54a
LeChayS <shinoacute@gmail.com>
2023-10-21 08:45:50 +0700

    da xong nav

--- css,jvs/headFoot.js
+++ css,jvs/headFoot.js
@@ -1,22 +1,22 @@
 const nav = document.querySelector(".nav"),
-    searchIcon = document.querySelector("#searchIcon"),
-    navOpenbtn = document.querySelector(".navOpenbtn"),
-    navClosebtn = document.querySelector(".navClosebtn");
+  searchIcon = document.querySelector("#searchIcon"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
 
 searchIcon.addEventListener("click", () => {
-    nav.classList.toggle("openSearch");
-    nav.classList.remove("openNav");
-    if(nav.classList. contains("openSearch")){
-        return  searchIcon.classList.replace("fi-br-search", "fi-br-cross");
-    }
-    return  searchIcon.classList.replace("fi-br-cross", "fi-br-search");
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    return searchIcon.classList.replace("fi-br-search", "fi-br-cross");
+  }
+  searchIcon.classList.replace("fi-br-cross", "fi-br-search");
 });
 
-navOpenbtn.addEventListener("click" , () => {
-    nav.classList.add("openNav");
-    nav.classList.remove("openSearch");
-    searchIcon.classList.replace("fi-br-cross", "fi-br-search");
-})
-navClosebtn.addEventListener("click" , () => {
-    nav.classList.remove("openNav");
-})
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("fi-br-cross", "fi-br-search");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
