9c355e462e616b0b397e85e9a02e5a6611291e52
LeChayS <shinoacute@gmail.com>
2023-11-03 14:40:59 +0700

    dang responsive nhung doi qua :(

--- css,jvs/header.js
+++ css,jvs/header.js
@@ -1,3 +1,7 @@
+window.addEventListener("scroll",function(){
+   var header = this.document.querySelector("header");
+   header.classList.toggle("sticky",window.scrollY > 0);
+})
 
 
  const menu = document.querySelector(".menu");
